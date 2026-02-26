cc7c0bf50b56fbbb2a3ed6b4cc1c120b39ba6cc4
isaiah-zimmerman <zimmermanit23@gcc.edu>
2026-02-26 14:10:44 -0500

    frontend outline

---
+++ Frontend/src/Base.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+
+function Base() {
+  return (
+    <div className='app-layout'>
+      {/* <Sidebar /> */}
+      <main className='main-content'>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default Base
