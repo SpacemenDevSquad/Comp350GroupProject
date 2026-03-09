a1ee20e62a806c6c4ed75297128e42a4d3411826
Peter Brumbach <contact@peterbrumbach.com>
2026-03-09 10:42:30 -0400

    A few changes:
    Added course container HTML element
    Added smooth transition when user hits enter
    Adjustable based on browser window size

--- Frontend/src/views/Home.jsx
+++ Frontend/src/views/Home.jsx
@@ -11,12 +11,44 @@ function Home() {
       <div id='content'>
         <h1 id='title'>GCC Course Search</h1>
         <div id="searchContainer">
-          <input type='search' id="searchBar" placeholder='Search Courses...'></input>
+          <input type='search' id="searchBar" placeholder='Search Courses...' onKeyDown={(e) => {OnHitEnter(e)}}></input>
           <button id="filterButton">☰</button>
         </div>
       </div>
+      <div id='courseContainer'></div>
     </div>
   );
 }
+
+async function OnHitEnter(e) {
+  if (e.key !== "Enter") return;
+  console.log("Searching for courses...")
+  
+  // TODO:
+  // Send search API call here to retrieve a list of courses
+  const courseResults = [];
+
+  // TODO:
+  // Change CSS to fit in new course elements
+  const titleText = document.getElementById("title");
+  const content = document.getElementById("content");
+  const courseContainer = document.getElementById("courseContainer");
+
+  titleText.style.opacity = '0%';
+  titleText.style.fontSize = '0vw';
+  titleText.style.height = '50px';
+  content.style.transform = 'translateY(calc(-50vh + 55px))';
+  courseContainer.style.top = '120px';
+  courseContainer.style.height = '1000px';
+  await delay(500);
+
+  
+
+  // TODO:
+  // Wait for course results, then create course HTML objects
+
+}
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 export default Home;
