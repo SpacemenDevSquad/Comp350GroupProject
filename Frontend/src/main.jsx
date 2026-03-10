3d0d69b7077675e0835cb1af80701e29f5a9a8a8
Peter Brumbach <contact@peterbrumbach.com>
2026-03-09 20:35:48 -0400

    Roughed in a service worker and PWA

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import './css/fonts.css'
 import App from './App.jsx'
+import './js/registerWorkers.js'
+import '../serviceWorker.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
