3b274974da3ad4e1bf26965bae9a76b93e48f7db
isaiah-zimmerman <zimmermanit23@gcc.edu>
2026-03-15 20:54:42 -0400

    Update TimeFilter.java

--- Backend/src/main/java/edu/gcc/prij/filters/TimeFilter.java
+++ Backend/src/main/java/edu/gcc/prij/filters/TimeFilter.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 import edu.gcc.prij.objects.section.Section;
 
 public class TimeFilter implements Filter {
-    // private 
+    /* 
+    {
+        M: [(480, 1080)]
+        T: [(480, 1080)]
+        W: [(480, 1080)]
+        R: [(480, 1080)]
+        F: [(480, 1080)]
+    }
+    
+    availableToggle - true if this is availability, false if this is not availability
+    */
 
     @Override
     public ArrayList<Section> filter(ArrayList<Section> currentCourses){
