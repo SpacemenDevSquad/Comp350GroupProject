bae54aa3540de22c277ba2ab3ab47afabccb9361
JamesYoho13 <YohoJR23@GCC.edu>
2026-04-15 10:44:48 -0400

    Changing default semester to Fall 2026

--- frontend/src/components/UpdateSemester.jsx
+++ frontend/src/components/UpdateSemester.jsx
@@ -4,7 +4,7 @@ function UpdateSemester({ year, setYear, term, setTerm }){
   return (
     <div>
       Update Semester
-      <select name="semester_year_selector" id="semester_year_selector" onChange={(e) => setYear(parseInt(e.target.value))} defaultValue={"2023"}>
+      <select name="semester_year_selector" id="semester_year_selector" onChange={(e) => setYear(parseInt(e.target.value))} defaultValue={"2026"}>
         <option value="2023">2023</option>
         <option value="2024">2024</option>
         <option value="2025">2025</option>
