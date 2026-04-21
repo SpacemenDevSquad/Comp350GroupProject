7a03bb3d7b25b80fc529a40cad16ae803449afd7
Peter Brumbach <contact@peterbrumbach.com>
2026-04-21 18:56:51 -0400

    Caching Files

--- frontend/serviceWorker.js
+++ frontend/serviceWorker.js
@@ -1,12 +1,68 @@
 const cacheName = "gccCourseSchedule - File Caching"
 const cachedFiles = [
-  "/index.html",
-  "/src/App.jsx"
+  "/",
+  "/manifest.json",
+  "/src/main.jsx",
+  "/src/App.jsx",
+  "/src/views/Home.jsx",
+  "/src/views/Schedule.jsx",
+  "/src/views/StatusSheet.jsx",
+  "/src/js/alertPop.js",
+  "/src/js/createAlert.jsx",
+  "/src/js/firebase.js",
+  "/src/js/screenTransitions.js",
+  "/src/js/searchBar.js",
+  "/src/js/toggleFilter.js",
+  "/src/images/AppleIcon.png",
+  "/src/images/backArrow.svg",
+  "/src/images/bg1.png",
+  "/src/images/bg2.png",
+  "/src/images/calendar.svg",
+  "/src/images/favicon.png",
+  "/src/images/logo.png",
+  "/src/images/PRIJ_horizontal_white.svg",
+  "/src/images/profile.svg",
+  "/src/images/search.svg",
+  "/src/images/searchIcon.svg",
+  "/src/fonts/robotoslab-variablefont_wght-webfont.woff",
+  "/src/fonts/robotoslab-variablefont_wght-webfont.woff2",
+  "/src/css/alert.css",
+  "/src/css/App.css",
+  "/src/css/Filters.css",
+  "/src/css/fonts.css",
+  "/src/css/Home.css",
+  "/src/css/index.css",
+  "/src/css/login.css",
+  "/src/css/requirements.css",
+  "/src/css/schedule.css",
+  "/src/css/section.css",
+  "/src/css/StatusSheet.css",
+  "/src/css/TopBar.css",
+  "/src/css/weeklySchedule.css",
+  "/src/components/Alert.jsx",
+  "/src/components/Course.jsx",
+  "/src/components/Filters.jsx",
+  "/src/components/RequirementGroup.jsx",
+  "/src/components/Section.jsx",
+  "/src/components/TopBar.jsx",
+  "/src/components/UpdateSemester.jsx",
+  "/src/components/WeeklySchedule.jsx",
+  "/@vite/client",
+  "/node_modules/vite/dist/client/env.mjs",
+  "/serviceWorker.js",
+  "/src/js/registerWorkers.js",
+  "/@react-refresh",
+  "/src/images/PRIJ_horizontal_white.svg?import"
 ]
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
 self.addEventListener('install', (event) => {
   console.log('Service worker installed');
+  self.skipWaiting();
+  self.clients.claim();
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
       console.log('Caching app files');
