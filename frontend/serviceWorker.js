cb700e39937a8a015186fc06b70dfceb713f1b4f
Peter Brumbach <contact@peterbrumbach.com>
2026-04-21 18:23:55 -0400

    Update serviceWorker.js

--- frontend/serviceWorker.js
+++ frontend/serviceWorker.js
@@ -1,3 +1,24 @@
+const cacheName = "gccCourseSchedule - File Caching"
+const cachedFiles = [
+  "/index.html",
+  "/src/App.jsx"
+]
+
+
 self.addEventListener('install', (event) => {
   console.log('Service worker installed');
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('Caching app files');
+      return cache.addAll(cachedFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
